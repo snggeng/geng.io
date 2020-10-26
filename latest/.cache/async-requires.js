39a9f9ff26dd56cffb25ec183827097baee99c79
Geng Sng <snggeng@gmail.com>
2020-10-26 00:05:50 -0700

    remove: deprecated

---
+++ latest/.cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/geng/projects/geng.io/latest/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/geng/projects/geng.io/latest/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-me-js": () => import("/Users/geng/projects/geng.io/latest/src/pages/about-me.js" /* webpackChunkName: "component---src-pages-about-me-js" */),
+  "component---src-pages-besides-work-js": () => import("/Users/geng/projects/geng.io/latest/src/pages/besides-work.js" /* webpackChunkName: "component---src-pages-besides-work-js" */),
+  "component---src-pages-index-js": () => import("/Users/geng/projects/geng.io/latest/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-posts-js": () => import("/Users/geng/projects/geng.io/latest/src/pages/posts.js" /* webpackChunkName: "component---src-pages-posts-js" */)
+}
+
